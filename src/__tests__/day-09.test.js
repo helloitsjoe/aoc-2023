362104e59c07ac106e24232a79c22d595667c302
Joe Boyle <jboyle617@gmail.com>
2024-01-30 23:37:31 -0500

    wip day 9

---
+++ src/__tests__/day-09.test.js
@@ -0,0 +1,42 @@
+import { getNextLevel, getNextInSequence } from '../day-09.js';
+
+// beforeEach(() => {
+//   globalThis.challengeNum = 2;
+// });
+
+// afterEach(() => {
+//   delete globalThis.challengeNum;
+// });
+
+describe('getNextLevel', () => {
+  it.each`
+    level                | nextLevel
+    ${[1, 1, 1]}         | ${[0, 0]}
+    ${[1, 2, 3]}         | ${[1, 1]}
+    ${[1, 3, 6, 10, 15]} | ${[2, 3, 4, 5]}
+  `('$level', ({ level, nextLevel }) => {
+    expect(getNextLevel(level)).toEqual(nextLevel);
+  });
+});
+
+describe('getNextInSequence', () => {
+  it.each`
+    sequence             | next
+    ${[1, 1, 1]}         | ${1}
+    ${[1, 2, 3]}         | ${4}
+    ${[1, 3, 6, 10, 15]} | ${21}
+  `('$sequence', ({ sequence, next }) => {
+    expect(getNextInSequence(sequence)).toEqual(next);
+  });
+});
+
+// describe('processSequences', () => {
+//   it('basic', () => {
+//     const input = [
+//       [0, 3, 6, 9, 12, 15],
+//       [1, 3, 6, 10, 15, 21],
+//       [10, 13, 16, 21, 30, 45],
+//     ];
+//     expect(processSequences(input)).toEqual([18, 28, 68]);
+//   });
+// });
--- src/day-09.js
+++ src/day-09.js
@@ -1,5 +1,43 @@
 import { parseCli } from './utils.js';
 
+export function getNextLevel(nums) {
+  let currentRound = [];
+  for (const [iKey, num] of nums.entries()) {
+    const i = Number(iKey);
+    if (nums[i + 1] != null) {
+      currentRound.push(nums[i + 1] - num);
+    }
+  }
+  return currentRound;
+}
+
+function assembleLevels(nums, levels = [nums]) {
+  if (nums.every((num) => num === 0)) {
+    return levels;
+  }
+  const nextLevel = getNextLevel(nums);
+  return assembleLevels(nextLevel, [...levels, nextLevel]);
+}
+
+export function getNextInSequence(sequence) {
+  const levels = assembleLevels(sequence);
+  console.log('levels', levels);
+
+  const lastNums = [0];
+  for (const [iKey, level] of Object.entries(levels)) {
+    const i = Number(iKey);
+    // console.log('level', level);
+    if (levels[i - 1] != null) {
+      lastNums.push(levels[i - 1].at(-1) + level.at(-1));
+    }
+    if (i === levels.length - 1) {
+      console.log('lastNums', lastNums);
+      console.log(' level', level);
+      return lastNums.at(-1);
+    }
+  }
+}
+
 // Part 1: Find the sum of all lines' next number
 export default function main() {
   const { dataType } = parseCli(process.argv);
