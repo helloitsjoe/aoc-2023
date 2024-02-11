21329e0b990124021ba2f0b9b39d5375dff0afa9
Joe Boyle <jboyle617@gmail.com>
2024-02-11 08:46:55 -0500

    day 11 wip

--- src/day-11.js
+++ src/day-11.js
@@ -2,6 +2,26 @@ import { parseCli, arrayify } from './utils.js';
 
 export function expand(arr) {
   const output = [];
+
+  for (let i = 0; i < arr[0].length; i++) {
+    if (arr[0][i] === '.') {
+      let allDots = true;
+      for (const line of arr) {
+        if (line[i] !== '.') {
+          allDots = false;
+          break;
+        }
+      }
+      if (allDots) {
+        // Push an extra column
+        for (const line of arr) {
+          line.splice(i, 0, '.');
+        }
+        i++;
+      }
+    }
+  }
+
   for (const line of arr) {
     if (line.every((el) => el === '.')) {
       output.push(line);
